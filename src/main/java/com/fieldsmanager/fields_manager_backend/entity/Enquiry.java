555d4dfaa46def73eba01ec9a846abb48f8afce3
YoussefNabil77 <youssefwaer145@gmail.com>
2025-08-29 03:57:21 +0300

    feat: add enquiry entity

---
+++ src/main/java/com/fieldsmanager/fields_manager_backend/entity/Enquiry.java
@@ -0,0 +1,43 @@
+package com.fieldsmanager.fields_manager_backend.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "enquiry")
+
+public class Enquiry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
